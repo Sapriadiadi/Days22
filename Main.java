9898ee0eebfdb15fc4535d3e021800f5925c3c76
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-30 23:23:24 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package days22;
+public class Main{
+static void myMethod() {
+System.out.println("1 adalah bilangan asli");
+}
+
+
+public static void main(String[] args) {
+    myMethod();
+
+}
+
+    
+}
+
+
+
+
+
+
+    
